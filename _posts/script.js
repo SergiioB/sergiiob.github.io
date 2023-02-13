8d276bb008a61cb94195094d7348103249206e89
SergiioB <sergiobarrientose@outlook.com>
2023-02-13 14:19:32 +0100

    a

--- _posts/script.js
+++ _posts/script.js
@@ -1,13 +1,17 @@
- function copyToClipboard() {
-        const text = document.getElementById("textToCopy").textContent;
-        const clipboard = new ClipboardJS('.btn', {
-          text: () => text
-        });
-        clipboard.on('success', function(e) {
-          e.clearSelection();
-          document.getElementById("copyStatus").textContent = "Copied!";
-        });
-        clipboard.on('error', function(e) {
-          console.error('Error copying text: ', e);
-        });
-      }
+  function copyToClipboard() {
+    const text = document.getElementById("textToCopy").textContent.trim();
+    const button = document.querySelector(".btn");
+    const clipboard = new ClipboardJS('.btn', {
+      text: () => text
+    });
+    clipboard.on('success', function(e) {
+      e.clearSelection();
+      button.value = "Copied!";
+      setTimeout(function() {
+        button.value = "Copy";
+      }, 3000);
+    });
+    clipboard.on('error', function(e) {
+      console.error('Error copying text: ', e);
+    });
+  }
